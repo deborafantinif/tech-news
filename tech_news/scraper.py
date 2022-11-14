ed80e76671f1e49dff54563856a511c0a1803363
deborafantinif <deboraferfan@gmail.com>
2022-11-14 11:12:37 -0300

    FEAT: info detalhada do post

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -38,7 +38,42 @@ def scrape_next_page_link(html_content):
 
 # Requisito 4
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    url = (
+        Selector(html_content)
+        .css("head link[rel='canonical']::attr(href)")
+        .get()
+    )
+    title = Selector(html_content).css("h1.entry-title::text").get()
+    timestamp = Selector(html_content).css("li.meta-date::text").get()
+    writer = Selector(html_content).css("span.author a::text").get()
+    comments_count_string = (
+        Selector(html_content).css("div.title-block h5::text").get()
+    )
+    if comments_count_string is not None:
+        comments_count = [
+            int(s) for s in comments_count_string.split() if s.isdigit()
+        ][0]
+    else:
+        comments_count = 0
+    summary = Selector(html_content).css("div.entry-content p::text").get()
+    find_tags = (
+        Selector(html_content).css("section.post-tags a::text").getall()
+    )
+    tags = []
+    if find_tags is not None:
+        tags = find_tags
+    category = Selector(html_content).css("span.label::text").get()
+
+    return {
+        "url": url,
+        "title": title,
+        "timestamp": timestamp,
+        "writer": writer,
+        "comments_count": comments_count,
+        "summary": summary.strip(),
+        "tags": tags,
+        "category": category,
+    }
 
 
 # Requisito 5
