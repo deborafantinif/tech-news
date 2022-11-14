90cea4517c35c7ca05e57ee6b2717125ba7152f0
deborafantinif <deboraferfan@gmail.com>
2022-11-14 18:09:16 -0300

    FEAT: encontra 5 categorias mais populares

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,3 +1,5 @@
+from collections import Counter
+
 from tech_news.database import get_collection
 
 
@@ -10,9 +12,31 @@ def top_5_news():
     return [(new["title"], new["url"]) for new in news_by_comments[:5]]
 
 
+def sorted_name(dict_sorted):
+    for index, value in enumerate(dict_sorted):
+        if dict_sorted[index][1] == dict_sorted[index - 1][1]:
+            if dict_sorted[index][0] < dict_sorted[index - 1][0]:
+                save = dict_sorted[index - 1]
+                dict_sorted[index - 1] = dict_sorted[index]
+                dict_sorted[index] = save
+    return dict_sorted
+
+
 # Requisito 11
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    news = list(get_collection().find({}))
+    all_categories = []
+    for new in news:
+        all_categories.append(new["category"])
+    dict_category = Counter(all_categories)
+    dict_sorted = sorted(
+        dict_category.items(), key=lambda pair: pair[1], reverse=True
+    )
+    dict_sorted = sorted_name(dict_sorted)
+    categories = []
+    for key in dict_sorted[:5]:
+        categories.append(key[0])
+    return categories
 
 
 def test():
