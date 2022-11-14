f537f9bc4eb80b48f6afea1a0511a4305d943390
deborafantinif <deboraferfan@gmail.com>
2022-11-14 16:14:16 -0300

    FEAT: pesquisa de noticias por titulo

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,15 @@
+from tech_news.database import search_news
+import re
+
+
 # Requisito 6
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    news = []
+    rgx = re.compile(f".*{title}.*", re.IGNORECASE)
+    news_found = search_news({"title": {"$regex": rgx}})
+    for new in news_found:
+        news.append((new["title"], new["url"]))
+    return news
 
 
 # Requisito 7
